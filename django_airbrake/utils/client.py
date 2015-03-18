18e15a5780d9c6b286e34963de2905280d0d5068
Hubert Piotrowski <hubert.piotrowski@docomointertoch.com>
2015-03-18 23:43:09 +0800

    removed unnecessary code

--- django_airbrake/utils/client.py
+++ django_airbrake/utils/client.py
@@ -7,13 +7,6 @@ from lxml import etree
 
 
 class Client(object):
-    API_URL = '%s://airbrake.io/notifier_api/v2/notices'
-    ERRORS = {
-        403: "Cannot use SSL",
-        422: "Invalid XML sent to Airbrake",
-        500: "Airbrake has braked too hard",
-    }
-
     DEFAULTS = {
         'TIMEOUT': 5,
         'USE_SSL': False,
@@ -27,6 +20,7 @@ class Client(object):
 
         return Client.API_URL % scheme
 
+
     @property
     def settings(self):
         if getattr(self, '_settings', None):
@@ -36,8 +30,8 @@ class Client(object):
         self._settings.update(getattr(settings, 'AIRBRAKE', {}))
         return self._settings
 
+
     def notify(self, exception=None, request=None):
-        print 'n'*80
         headers = {
             'Content-Type': 'text/xml'
         }
@@ -45,88 +39,7 @@ class Client(object):
             air_log = airbrake.getLogger(api_key = self.settings.AIRBRAKE['API_KEY'],
                                         project_id = self.settings.AIRBRAKE['PROJECT_ID'],
                                          environment = self.settings.AIRBRAKE['ENVIRONMENT'])
-            """
-            payload = self._generate_xml(exception=exception, request=request)
-            print '9'*10
-            req = urllib2.Request(self.url, payload, headers)
-            print '8'*10
-            resp = urllib2.urlopen(req, timeout=self.settings['TIMEOUT'])
-            print '7'*10
-            status = resp.getcode()
-            print '6'*10
-            """
             air_log.exception(str(exception))
         except Exception ,e:
             print 'Airbrake exception: %s\n\n%s' % (e, traceback.format_exc())
-        #print '-'*80
-        #print payload
-        #print 's'*80, status
-        #if status == 200:
-        #    return True
-        #elif status in Client.ERRORS:
-        #    raise Exception(Client.ERRORS[status])
 
-    def _generate_xml(self, exception=None, request=None):
-        _,_,trace = sys.exc_info()
-        notice_em = etree.Element('notice', version='2.0')
-
-        tb = traceback.extract_tb(trace)
-
-        api_key = etree.SubElement(notice_em, 'api-key').text = self.settings['API_KEY']
-
-        notifier_em = etree.SubElement(notice_em, 'notifier')
-
-        etree.SubElement(notifier_em, 'name').text = 'django-airbrake'
-        etree.SubElement(notifier_em, 'version').text = '0.0.2'
-        etree.SubElement(notifier_em, 'url').text = 'http://example.com'
-
-        if request:
-            request_em = etree.SubElement(notice_em, 'request')
-
-            if request.is_secure():
-                scheme = 'https'
-            else:
-                scheme = 'http'
-            url = '%s://%s%s' % (scheme, request.get_host(),
-                request.get_full_path())
-            etree.SubElement(request_em, 'url').text = str(url)
-
-            cb,_,_ = resolve(request.path)
-            etree.SubElement(request_em, 'component').text = str(cb.__module__)
-            etree.SubElement(request_em, 'action').text = str(cb.__name__)
-
-            if len(request.POST):
-                params_em = etree.SubElement(request_em, 'params')
-
-                for key, val in request.POST.items():
-                    var = etree.SubElement(params_em, 'var')
-                    var.set('key', str(key))
-                    var.text = str(val)
-
-            session = request.session.items()
-            if len(session):
-                session_em = etree.SubElement(request_em, 'session')
-                for key, val in session:
-                    var = etree.SubElement(session_em, 'var')
-                    var.set('key', str(key))
-                    var.text = str(val)
-
-            if exception:
-                error_em = etree.SubElement(notice_em, 'error')
-
-                etree.SubElement(error_em, 'class').text = str(exception.__class__.__name__)
-                etree.SubElement(error_em, 'message').text = str(exception)
-
-                backtrace_em = etree.SubElement(error_em, 'backtrace')
-
-                for line in tb:
-                    etree.SubElement(backtrace_em, 'line',
-                        file=str(line[0]),
-                        number=str(line[1]),
-                        method=str(line[2]))
-
-        env_em = etree.SubElement(notice_em, 'server-environment')
-
-        etree.SubElement(env_em, 'environment-name').text = getattr(settings, 'ENVIRONMENT', 'development')
-
-        return '<?xml version="1.0" encoding="UTF-8"?>%s' % etree.tostring(notice_em)
